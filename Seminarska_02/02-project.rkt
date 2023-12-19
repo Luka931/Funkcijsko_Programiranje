57d7cf3b8e778f2387203629dbe57087478355e2
lukap <lukapavicevic5@gmail.com>
2023-12-19 04:11:37 +0100

    Checkpoint before implementing Environments

--- Seminarska_02/02-project.rkt
+++ Seminarska_02/02-project.rkt
@@ -31,11 +31,14 @@
 (define (isBool? e)
     (or (true? e) (false? e)))
 
-(define (isSeq? seq) (let*
-    ([e2 (..-e2 seq)])
-    (if (..? e2)
-        (isSeq? e2)
-        (empty? e2))))
+(define (isSeq? seq) 
+    (if (..? seq) 
+        (let*
+        ([e2 (..-e2 seq)])
+        (if (..? e2)
+            (isSeq? e2)
+            (empty? e2)))
+        #f))
 
 (define (addInt e1 e2)
     (int (+ (int-int e1) (int-int e2))))
@@ -76,62 +79,6 @@
 (define (seqLeq? e1 e2)
     (<= (..Len e1) (..Len e2)))
 
-; All of the functions for Equalities
-
-(define (exceptionEq? e1 e2) (let*
-    ([e1Text (exception-e e1)]
-     [e2Text (exception-e e2)])
-    (string=? e1Text e2Text)))
-
-(define (triggeredEq? e1 e2) (let*
-    ([e1Ex (triggered-e e1)]
-     [e2Ex (triggered-e e2)])
-    (exceptionEq?  e1Ex e2Ex)))
-
-(define (triggerEq? e1 e2)(let*
-    ([e1Ex (trigger-e e1)]
-     [e2Ex (trigger-e e2)])
-    (exceptionEq?  e1Ex e2Ex)))
-
-(define (intEq? e1 e2)
-    (= (int-int e1) (int-int e2)))
-
-(define (boolEq? e1 e2)
-    (or (and (true? e1) (true? e2))
-        (and (false? e1) (false? e2))))
-
-(define (..Eq? e1 e2)(let* 
-    ([e1e1 (..-e1 e1)]
-     [e1e2 (..-e2 e1)]
-     [e2e1 (..-e1 e2)]
-     [e2e2 (..-e2 e2)]
-     [firstsEq (universalEq? e1e1 e2e1)]
-     [secondsEq (universalEq? e1e2 e2e2)])
-    (if (or (string? firstsEq) (string? secondsEq))
-        "Erorr"
-    (if (and firstsEq secondsEq)
-        #f
-    (if (or (empty? e1e2) (empty? e2e2))
-        #f
-        "Erorr")))))
-
-(define (universalEq? e1 e2)
-    (if (and (int? e1) (int? e2))
-        (intEq? e1 e2)
-    (if (and (isBool? e1) (isBool? e2))
-        (boolEq? e1 e2)
-    (if (and (..? e1) (..? e2))
-        (..Eq? e1 e2)
-    (if (and (empty? e1) (empty? e2))
-        #t
-    (if (and (exception? e1) (exception? e2))
-        (exceptionEq? e1 e2)
-    (if (and (trigger? e1) (trigger? e2))
-        (triggerEq? e1 e2)
-    (if (and (triggered? e1) (triggered? e2))
-        (triggeredEq? e1 e2)
-        "Erorr"))))))))
-
 (define (int~ e)
     (int (* -1 (int-int e))))
 
@@ -159,7 +106,6 @@
         #f))))
         
 
-
 (define (fri e)
     (cond
         [(int? e) e]
@@ -177,13 +123,11 @@
             ([e1 (fri (handle-e1 e))]
              [e2 (fri (handle-e2 e))]
              [e3 (handle-e3 e)])
-            (begin
-                [if (triggered? e1)
-                     e1
-                    (e1 (triggered (exception "trigger: wrong argument type")))]
-                [if (triggeredEq? e1 e2)
-                    (fri e3)
-                    e2]))]
+            (if (not (triggered? e1))
+                (triggered (exception "trigger: wrong argument type"))
+            (if (equal? e1 e2)
+                (fri e3)
+                e2)))]
         [(if-then-else? e) (letrec
             ([condition (fri (if-then-else-condition e))]
              [e1 (if-then-else-e1 e)]
@@ -230,7 +174,7 @@
                 (addBool e1 e2)
             (if (and (isSeq? e1) (isSeq? e2))
                 (addSeq e1 e2)
-                (triggered (exception("add: wrong argument type")))))))]
+                (triggered (exception "add: wrong argument type"))))))]
         [(mul? e)(letrec
             ([e1 (fri (mul-e1 e))]
              [e2 (fri (mul-e2 e))])
@@ -238,7 +182,7 @@
                 (mulInt e1 e2)
             (if (and (isBool? e1) (isBool? e2))
                 (mulBool e1 e2)
-                (triggered (exception("mul: wrong argument type"))))))]
+                (triggered (exception "mul: wrong argument type")))))]
         [(?leq? e)(letrec
             ([e1 (fri (?leq-e1 e))]
              [e2 (fri (?leq-e2 e))])
@@ -248,49 +192,76 @@
                 (boolLeq? e1 e2)
             (if (and (isSeq? e1) (isSeq? e2))
                 (seqLeq? e1 e2)
-                (triggered (exception("?leq: wrong argument type")))))))]
+                (triggered (exception "?leq: wrong argument type"))))))]
         [(?=? e)(letrec
             ([e1 (fri (?=-e1 e))]
-             [e2 (fri (?=-e2 e))]
-             [result (universalEq? e1 e2)])
-            (if (string? result)
-                (triggered (exception("?leq: wrong argument type")))
-                result))]
+             [e2 (fri (?=-e2 e))])
+            (if (equal? e1 e2)
+                (true)
+                (false)))]
         [(head? e)
             (if (..? e)
                 (..-e1 e)
             (if (empty? e)
-                (triggered (exception("head: empty sequence")))
-                (triggered (exception("head: wrong argument type")))))]
+                (triggered (exception "head: empty sequence"))
+                (triggered (exception "head: wrong argument type"))))]
 
         [(tail? e)
             (if (..? e)
                 (..-e2 e)
             (if (empty? e)
-                (triggered (exception("tail: empty sequence")))
-                (triggered (exception("tail: wrong argument type")))))]
+                (triggered (exception "tail: empty sequence"))
+                (triggered (exception "tail: wrong argument type"))))]
         [(~? e)(letrec
             ([evaluated (fri (~-e e))])
             (if (isBool? evaluated)
                 (bool~ evaluated)
             (if (int? evaluated)
                 (int~ evaluated)
-                (triggered (exception("~: wrong argument type"))))))]
+                (triggered (exception "~: wrong argument type")))))]
         [(?all? e)(letrec
             ([evaluated (fri (?all-e e))])
             (if (isSeq? evaluated)
                 (if (not (seqContainsFalse? evaluated))
                     (true)
                     (false))
-                (triggered (exception("?all: wrong argument type")))))]
+                (triggered (exception "?all: wrong argument type"))))]
         [(?any? e)(letrec
             ([evaluated (fri (?any-e e))])
             (if (isSeq? evaluated)
                 (if (not (seqContainsAllFalse? evaluated))
                     (true)
                     (false))
-                (triggered (exception("?any: wrong argument type")))))]
+                (triggered (exception "?any: wrong argument type"))))]
 
         
+
+        [#t (triggered (exception "Bad Expression."))]
         ))
 
+
+(define (revHepler seq acc) (let* 
+    ([e1 (..-e1 seq)]
+     [e2 (..-e2 seq)])
+    (if (empty? e2)
+        (.. e1 acc)
+        (revHepler e2 (.. e1 acc)))))
+
+(define (rev e) 
+    (revHepler e (empty))
+)
+
+
+(define (binaryHelper num acc)(let 
+    ([B0 (int 0)]
+     [B1 (int 1)])
+    (if (equal? num 0)
+        acc
+    (if (equal? (remainder num 2) 1)
+        (binaryHelper (quotient num 2) (.. B1 acc))
+        (binaryHelper (quotient num 2) (.. B0 acc))))))
+    
+(define (binary e)
+    (binaryHelper e (empty)))
+
+(provide (all-defined-out))
---
+++ Seminarska_02/projectTests.rkt
@@ -0,0 +1,100 @@
+#lang racket
+
+(require rackunit (prefix-in pr: "02-project.rkt"))
+
+(define TRIGGER_WRONG_ARGUMENT (pr:triggered (pr:exception "trigger: wrong argument type")))
+
+
+(define t (pr:true))
+(define f (pr:false))
+(test-case "Booleans"
+    (check-equal? (pr:fri t) t "True results in true.")
+    (check-equal? (pr:fri f) f "False results in flase.")
+)
+
+(define iPos (pr:int 12))
+(define iNeg (pr:int -12))
+(test-case "Integers"
+    (check-equal? (pr:fri iPos) iPos "Positive intiger")
+    (check-equal? (pr:fri iNeg) iNeg "Negative intiger")
+)
+
+(define listT (pr:.. iPos (pr:.. iNeg (pr:.. t (pr:.. f f)))))
+(define seqT (pr:.. iPos (pr:.. iNeg (pr:.. t (pr:.. f (pr:empty))))))
+(test-case "List, Sequence, and Empty"
+    (check-equal? (pr:fri listT) listT "List is returned")
+    (check-equal? (pr:fri (pr:empty)) (pr:fri (pr:empty)) "Empty is returned")
+    (check-equal? (pr:fri seqT) seqT "Sequence is returned")
+)
+
+(define excpStr (pr:exception "My exception"))
+(define excpInt (pr:exception 12))
+(define excpFri.. (pr:exception listT))
+(test-case "Exceptions"
+    (check-equal? (pr:fri excpStr) excpStr "Exception with a String")
+    (check-equal? (pr:fri excpInt) excpInt "Exception with an Int")
+    (check-equal? (pr:fri excpFri..) excpFri.. "Exception with a List")
+)
+
+(define trigger1 (pr:trigger excpStr))
+(define trigger2 (pr:trigger excpFri..))
+(define trigger3 (pr:trigger (pr:int 12)))
+(test-case "Triggers"
+    (check-equal? (pr:fri trigger1) (pr:triggered excpStr) "String exception should be triggered")
+    (check-equal? (pr:fri trigger2) (pr:triggered excpFri..) "List exception should be triggered")
+    (check-equal? (pr:fri trigger3) TRIGGER_WRONG_ARGUMENT "Trigger wrong argument should be returned")
+)
+
+(define handle1 (pr:handle trigger1 trigger1 seqT))
+(define handle2 (pr:handle trigger1 trigger2 seqT))
+(define handle3 (pr:handle (pr:int 23) trigger1 seqT))
+(test-case "Handles"
+    (check-equal? (pr:fri handle1) seqT "Successfully handled exception.")
+    (check-equal? (pr:fri handle2) (pr:triggered excpFri..) "Unsuccessfully handled exception.")
+    (check-equal? (pr:fri handle3) TRIGGER_WRONG_ARGUMENT "Handled exception is not right format.")
+)
+
+(define intT (pr:int 43))
+(define seqF (pr:exception "Lalala"))
+(define badEx (pr:add "sd" "sdf"))
+(define ite1 (pr:if-then-else t intT seqF))
+(define ite2 (pr:if-then-else f intT seqF))
+(define ite3 (pr:if-then-else t intT badEx))
+(define ite4 (pr:if-then-else f badEx intT))
+(test-case "If then else"
+    (check-equal? (pr:fri ite1) intT "True branchworks")
+    (check-equal? (pr:fri ite2) seqF "False branch works")
+    (check-equal? (pr:fri ite3) intT "Code doesn't breake even though we have bad expresion in false branch.")
+    (check-equal? (pr:fri ite4) intT "Code doesn't breake even though we have bad expresion in true branch.")  
+)
+
+(define int?t (pr:?int (pr:if-then-else t (pr:int 12) t)))
+(define int?f (pr:?int (pr:if-then-else f (pr:int 12) t)))
+(test-case "?int"
+    (check-equal? (pr:fri int?t) (pr:true) "?int should return (true) for int.")
+    (check-equal? (pr:fri int?f) (pr:false) "?int should return (false) for non int.")
+)
+
+(define true?t (pr:?bool (pr:if-then-else f (pr:int 12) t)))
+(define false?t (pr:?bool (pr:if-then-else f (pr:int 12) f)))
+(define bool?f (pr:?bool (pr:if-then-else t (pr:int 12) t)))
+(test-case "?int"
+    (check-equal? (pr:fri true?t) (pr:true) "?bool should return (true) for (true)")
+    (check-equal? (pr:fri false?t) (pr:true) "?bool should return (true) for (false)")
+    (check-equal? (pr:fri bool?f) (pr:false) "?bool should return (false) for non bool.")
+)
+
+(define ..?t1 (pr:?.. (pr:if-then-else f (pr:int 12) (pr:.. (pr:int 1) (pr:int 1)))))  
+(define ..?t2 (pr:?.. (pr:if-then-else f (pr:int 12) (pr:.. (pr:int 1) (pr:empty)))))
+(define ..?f (pr:?.. (pr:if-then-else t (pr:int 12) (pr:.. (pr:int 1) (pr:int 1)))))
+(test-case "?.."
+    (check-equal? (pr:fri ..?t1) (pr:true) "?.. should return (true) for ..")
+    (check-equal? (pr:fri ..?t2) (pr:true) "?.. should return (true) for seq")
+    (check-equal? (pr:fri ..?f) (pr:false) "?.. should return (false) for non ../seq.")
+)
+
+
+
+
+
+
